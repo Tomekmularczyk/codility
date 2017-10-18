87c857c04f5a9588b36911fcf2eb5e11d0c1b511
TomekMularczyk <tomasz.mularczyk@risendot.com>
2017-10-18 09:42:58 +0200

    - merge sort implemented

--- __tests__/algorithms/merge_sort.js
+++ __tests__/algorithms/merge_sort.js
@@ -1,24 +1,19 @@
-const {mergeArrays, mergeSort} = require('../../src/algorithms/merge_sort');
+const { mergeArrays, mergeSort } = require('../../src/algorithms/merge_sort');
 
 it('merges sorted arrays correctly', () => {
-    const targetArr1 = [8, 6, 3, 1, 7, 2, 9];
-    const leftA1 = [3, 6, 8, 9];
-    const rightA1 = [1, 2, 7];
-    mergeArrays(targetArr1, leftA1, rightA1);
+  const leftA1 = [3, 6, 8, 9];
+  const rightA1 = [1, 2, 7];
 
-    expect(targetArr1).toEqual([1, 2, 3, 6, 7, 8, 9]);
+  expect(mergeArrays(leftA1, rightA1)).toEqual([1, 2, 3, 6, 7, 8, 9]);
 
-    const targetArr2 = [8, 2];
-    const leftA2 = [8];
-    const rightA2 = [2];
-    mergeArrays(targetArr2, leftA2, rightA2);
+  const leftA2 = [8];
+  const rightA2 = [2];
 
-    expect(targetArr2).toEqual([2, 8]);
+  expect(mergeArrays(leftA2, rightA2)).toEqual([2, 8]);
 });
 
 it('sorts correctly', () => {
-    const targetArr1 = [8, 6, 3, 1, 7, 2, 9];
-    const s = mergeSort(targetArr1);
-
-    expect(s).toEqual([1, 2, 3, 6, 7, 8, 9]);
+  expect(mergeSort([8, 6, 3, 1, 7, 2, 9])).toEqual([1, 2, 3, 6, 7, 8, 9]);
+  expect(mergeSort([-1])).toEqual([-1]);
+  expect(mergeSort([0, 0, 1, 0,])).toEqual([0, 0, 0, 1]);
 });
--- src/algorithms/merge_sort.js
+++ src/algorithms/merge_sort.js
@@ -6,38 +6,32 @@
 /**
  * Ta część mergująca zakłada że lewa tablica i prawa jest posortowana, i przez porównywanie elementów mergują się już od najmniejszego do najwiekszego
  */
-function mergeArrays(arr, leftArray, rightArray) {
-    let i = 0;
-    let j = 0;
-    let k = 0;
-    while (i < leftArray.length && j < rightArray.length) {
-        if (leftArray[i] <= rightArray[j]) {
-            arr[k] = leftArray[i];
-            i++;
-        } else {
-            arr[k] = rightArray[j];
-            j++;
-        }
-        k++;
+function mergeArrays(leftArray, rightArray) {
+  const arr = [];
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] <= rightArray[j]) {
+      arr[k] = leftArray[i++];
+    } else {
+      arr[k] = rightArray[j++];
     }
-    for (; i < leftArray.length; i++, k++) {
-        arr[k] = leftArray[i];
-    }
-    for (; j < rightArray.length; j++, k++) {
-        arr[k] = leftArray[j];
-    }
+    k++;
+  }
+
+  return arr.concat(leftArray.slice(i)).concat(rightArray.slice(j)); // add whats left (only one array may have items)
 }
 
 function mergeSort(arr) {
-    if (arr.length < 2) {
-        return arr;
-    }
-
-    const middle = Math.round(arr.length / 2);
-    const leftArr = mergeSort(arr.slice(0, middle));
-    const rightArr = mergeSort(arr.slice(middle));
-    mergeArrays(arr, leftArr, rightArr);
+  if (arr.length < 2) {
     return arr;
+  }
+
+  const middle = Math.round(arr.length / 2);
+  const leftArr = arr.slice(0, middle);
+  const rightArr = arr.slice(middle);
+  return mergeArrays(mergeSort(leftArr), mergeSort(rightArr));
 }
 
 module.exports.mergeArrays = mergeArrays;
