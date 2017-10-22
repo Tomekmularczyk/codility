ebbe0c90e8dc0f3d82589baceff0ac5e585b3d64
Tomek <tomaszmularczyk89@gmail.com>
2017-10-22 14:50:13 +0200

    - MaxHeap

--- __tests__/data_structures/MinHeap.test.js
+++ __tests__/data_structures/MinHeap.test.js
@@ -3,23 +3,30 @@ const MinHeap = require('../../src/data_structures/MinHeap').MinHeap;
 const emptyHeap = new MinHeap();
 const heap = new MinHeap();
 beforeAll(() => {
-    heap.addElement(1);
-    heap.addElement(2);
-    heap.addElement(3);
-    heap.addElement(4);
+    heap.add(1);
+    heap.add(3);
+    heap.add(2);
+    heap.add(4);
+    heap.add(6);
+    heap.add(5);
 
-});
-
-it('tracks size of heap', () => {
-    expect(emptyHeap.size()).toEqual(0);
-    expect(heap.size()).toEqual(4);
 });
 
 it('finds position of elements', () => {
-    expect(heap.getLeftChildIndex(0)).toEqual(1);
-    expect(heap.getRightChildIndex(0)).toEqual(2);
-    expect(heap.getLeftChildIndex(1)).toEqual(3);
-    expect(heap.getRightChildIndex(1)).toEqual(4);
-    expect(heap.getLeftChildIndex(2)).toEqual(5);
-    expect(heap.getRightChildIndex(2)).toEqual(6);
+    expect(heap.getLeftChild(0)).toEqual(1);
+    expect(heap.getRightChild(0)).toEqual(2);
+    expect(heap.getLeftChild(1)).toEqual(3);
+    expect(heap.getRightChild(1)).toEqual(4);
+    expect(heap.getLeftChild(2)).toEqual(5);
+    expect(heap.getRightChild(2)).toEqual(6);
+});
+
+it('polls element', () => {
+    heap.removeHead();
+    expect(heap.getLeftChild(0)).toEqual(1);
+    expect(heap.getRightChild(0)).toEqual(2);
+    expect(heap.getLeftChild(1)).toEqual(3);
+    expect(heap.getRightChild(1)).toEqual(4);
+    expect(heap.getLeftChild(2)).toEqual(5);
+    expect(heap.getRightChild(2)).toEqual(6);
 });
--- src/data_structures/MaxHeap.js
+++ src/data_structures/MaxHeap.js
@@ -1,26 +1,28 @@
 /**
- * Heap przechowuje elementy i dzieci w tablicy. Pierwszy element jest najmniejszy.
- * Przydaje się przy szybkim znajdywaniu min elementów oraz szybkim wstawianiu.
+ * Heap przechowuje elementy i dzieci w tablicy. Pierwszy element jest największy.
+ * Przydaje się przy szybkim znajdywaniu max elementów oraz szybkim wstawianiu.
  * Oblicza gdzie znajdują się dzieci za pomocą wzorów.
  * Przy dodawaniu dodajemy element do końca listy i przesówamy go w górę do odpowiedniego miejsca.
  * Przy usuwaniu ostatni element trafia na pierwsze miejsce i przesuwamy elementy w dół by go posortować
  *
  */
 
-class MinHeap {
+class MaxHeap {
     constructor() {
         this.array = [];
     }
 
     add(data) {
-        if (data === undefined) { throw "data must be valid to add"; }
+        if (data === undefined) {
+            throw "data must be valid to add";
+        }
 
         this.array.push(data);
         this.bubbleUp(this.array.length - 1, data);
     };
 
     bubbleUp(childIndex, childData) {
-        if(childIndex > 0) {
+        if (childIndex > 0) {
             const parentIndex = this.getParentIndex(childIndex);
             const parentData = this.array[parentIndex];
 
@@ -36,46 +38,47 @@ class MinHeap {
         return Math.floor((childIndex - 1) / 2);
     };
 
-
     removeHead() {
         const headNode = this.array[0];
         const tailNode = this.array.pop();
+
         this.array[0] = tailNode;
         this.bubbleDown(0, tailNode);
+
         return headNode;
     };
 
     shouldSwap(childData, parentData) {
-        return childData < parentData;
+        return childData > parentData;
     };
 
     bubbleDown(parentIndex, parentData) {
-        if(parentIndex < this.array.length) {
+        if (parentIndex < this.array.length) {
             let targetIndex = parentIndex;
             let targetData = parentData;
 
             const leftChildIndex = this.getLeftChild(parentIndex);
             const rightChildIndex = this.getRightChild(parentIndex);
 
-            if(leftChildIndex < this.array.length) {
+            if (leftChildIndex < this.array.length) {
                 const leftChildData = this.array[leftChildIndex];
 
-                if (this.shouldSwap( leftChildData, targetData )) {
+                if (this.shouldSwap(leftChildData, targetData)) {
                     targetIndex = leftChildIndex;
                     targetData = leftChildData;
                 }
             }
 
-            if(rightChildIndex < this.array.length) {
+            if (rightChildIndex < this.array.length) {
                 const rightChildData = this.array[rightChildIndex];
 
-                if(this.shouldSwap(rightChildData, targetData )) {
+                if (this.shouldSwap(rightChildData, targetData)) {
                     targetIndex = rightChildIndex;
                     targetData = rightChildData;
                 }
             }
 
-            if(targetIndex !== parentIndex) {
+            if (targetIndex !== parentIndex) {
                 this.array[parentIndex] = targetData;
                 this.array[targetIndex] = parentData;
                 this.bubbleDown(targetIndex, parentData);
@@ -87,10 +90,7 @@ class MinHeap {
         return parentIndex * 2 + 1;
     };
 
-    getRightChild(parentIndex){
+    getRightChild(parentIndex) {
         return parentIndex * 2 + 2;
     };
 }
-
-
-module.exports.MinHeap = MinHeap;
