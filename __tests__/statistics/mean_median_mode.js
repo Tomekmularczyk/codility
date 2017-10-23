be6d47938fb661e62bd995b28dcd420cb0f95792
Tomek <tomaszmularczyk89@gmail.com>
2017-10-23 19:29:51 +0200

    - mean mode median

---
+++ __tests__/statistics/mean_median_mode.js
@@ -0,0 +1,35 @@
+const {findMean, findMedian, findMode} = require('../../src/statistics/mean_median_mode');
+
+const emptyArr = [];
+let arr1;
+let arr2;
+beforeAll(() => {
+    arr1 = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060];
+    arr2 = [4, 4, 4, 3, 3, 3, 2, 1];
+});
+
+it('finds mean', () => {
+    const meanArr1 = findMean(arr1);
+    const meanEmptyArr = findMean(emptyArr);
+
+    expect(meanArr1).toEqual(43900.6);
+    expect(meanEmptyArr).toBeFalsy();
+});
+
+it('finds median', () => {
+    const medianArr1 = findMedian(arr1);
+    const medianEmptyArr = findMedian(emptyArr);
+
+    expect(medianArr1).toEqual(44627.5);
+    expect(medianEmptyArr).toBeFalsy();
+});
+
+it('finds mode', () => {
+    const modeArr1 = findMode(arr1);
+    const modeEmptyArr = findMode(emptyArr);
+    const medianArr2 = findMode(arr2);
+
+    expect(modeArr1).toEqual(4978);
+    expect(medianArr2).toEqual(3);
+    expect(modeEmptyArr).toBeFalsy();
+});
