11d135ee2a62684e6b4a7c493b0773e909a51fab
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2020-02-29 14:29:57 +0100

    update script

--- run.js
+++ run.js
@@ -1,22 +1,29 @@
 const path = require("path");
 const chokidar = require("chokidar");
+const chalk = require("chalk");
 const clearModule = require("clear-module");
 
+// clear terminal
 process.stdout.write("\033c");
+
+const getFormattedTime = () => {
+  const date = new Date();
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
 
 // One-liner for current directory
 const watcher = chokidar
   .watch("./src", {
     ignored: /test.js/
   })
-  .on("ready", () => console.log("Scan complete. Ready for changes..."));
+  .on("ready", () =>
+    console.log(chalk.underline("Scan complete. Ready for changes..."))
+  );
 
 watcher.on("change", filePath => {
-  const date = new Date();
-  const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const time = getFormattedTime();
   const fullPath = path.join(__dirname, filePath);
-
-  console.log(`${time} - ${filePath} has been changed`);
+  console.log(chalk.gray(`${time} - ${filePath} has been changed`));
 
   require(fullPath);
   clearModule(fullPath);
